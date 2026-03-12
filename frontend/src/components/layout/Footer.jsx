de97825e8b467af25df001eb5ff809f5bc1153a9
V4NNO <sacaliucvanea@gmail.com>
2026-03-12 12:25:26 +0200

    am adaugat home page

---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <h2>Smart Hotel | Hotel Room Booking System</h2>
+      <p>
+        ©2026 Smart Hotel — Developed By{" Ion, Artur, Cristi "}
+      </p>
+    </footer>
+  );
+}
