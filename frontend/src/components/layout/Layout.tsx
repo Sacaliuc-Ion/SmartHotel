956ca2888865c4e62844e1bca9484b8e1adead15
crudu-cristian <cristicr1356@gmail.com>
2026-03-29 23:33:41 +0300

    am adaugat layout

---
+++ frontend/src/components/layout/Layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode, useState } from 'react';
+import { Topbar } from './Topbar';
+import { Sidebar } from './Sidebar';
+
+export const Layout = ({ children }: { children: ReactNode }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+  return (
+    <div className="h-screen flex flex-col bg-gray-50">
+      <Topbar onToggleSidebar={() => setSidebarOpen((v) => !v)} sidebarOpen={sidebarOpen} />
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar isOpen={sidebarOpen} />
+        <main className="flex-1 overflow-y-auto p-8">{children}</main>
+      </div>
+    </div>
+  );
+};
