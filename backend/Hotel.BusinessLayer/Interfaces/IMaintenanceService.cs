5601ee9b70c2378b571b9724acbe727774a0fd26
NichiticiArtur <nichiticiartur@gmail.com>
2026-04-16 16:36:48 +0300

    Am implementat endpointuri pentru mentenanta

---
+++ backend/Hotel.BusinessLayer/Interfaces/IMaintenanceService.cs
@@ -0,0 +1,12 @@
+﻿namespace Hotel.BusinessLayer.Interfaces;
+
+using Hotel.BusinessLayer.Structure;
+using Hotel.Domain.Models.Maintenance;
+
+public interface IMaintenanceService
+{
+     Task<ServiceResult<List<TicketDto>>> GetTicketsAsync();
+     Task<ServiceResult> AcceptTicketAsync(int ticketId, int assignedUserId);
+     Task<ServiceResult> ResolveTicketAsync(int ticketId, int assignedUserId);
+     Task<ServiceResult<TicketDto>> CreateTicketAsync(CreateTicketRequest request, int reportedByUserId);
+}
